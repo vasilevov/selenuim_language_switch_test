54fe51183e3abd5e42d48d447343a7dce2340823
Oleg <vasilevovv@yandex.ru>
2024-05-21 12:27:23 +0300

    Добвил assert

--- test_items.py
+++ test_items.py
@@ -4,4 +4,7 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_check_basket_button(browser):
     browser.get(link)
-    browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    try:
+        browser.find_element(By.CSS_SELECTOR, '.btn-add-to-basket')
+    except Exception:
+        assert False, "Кнопка не найдена"
